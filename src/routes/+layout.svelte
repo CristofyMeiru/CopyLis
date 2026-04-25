bd4fba2326aca3d822001778266767d72325a4dc
Cristofy Meirú <meiru.cristofy05@gmail.com>
2026-04-25 16:53:59 -0300

    feat: initial project structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  let { children } = $props();
+</script>
+
+<main
+  data-tauri-drag-region
+  class=" p-4 border border-neutral-600 text-white bg-neutral-900 h-screen"
+>
+  {@render children?.()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "../global.css";
+</script>
+
+<div class=" ">
+  <h1 class="  text-lg">Clipboard</h1>
+  
+</div>
